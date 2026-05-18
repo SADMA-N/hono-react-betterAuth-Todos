aa50d95416ed0e2ea593370ff7dfd4d0b9fb3130
SADMA-N <185231556+SADMA-N@users.noreply.github.com>
2026-05-18 15:47:14 +0600

    edit: disable todo link when not signin

--- server/index.ts
+++ server/index.ts
@@ -1,19 +1,12 @@
 import { Hono } from "hono";
-import { getTodos } from "./db/queries";
 import { auth } from "./lib/auth";
+import { todos } from "./routes/todo.routes";
 
 const app = new Hono().basePath("/api");
 
 const route = app
   .on(["POST", "GET"], "/auth/*", (c) => auth.handler(c.req.raw))
-  .get("/todos", async (c) => {
-    try {
-      const todos = await getTodos();
-      return c.json(todos);
-    } catch (err) {
-      return c.json({ error: "Failed to fetch todos" }, 500);
-    }
-  })
+  .route("/todos", todos)
   .get("/people", (c) => {
     return c.json([
       { id: 1, name: "Turag" },
@@ -21,5 +14,6 @@ const route = app
       { id: 3, name: "Charlie" },
     ]);
   });
+
 export type AppType = typeof route;
 export default app;
---
+++ server/routes/todo.routes.ts
@@ -0,0 +1,18 @@
+import { Hono } from "hono";
+import { getTodosByUserID } from "../db/queries";
+import { authMiddleware } from "../middleware/auth.middleware";
+import { HonoEnv } from "../type";
+
+export const todos = new Hono<HonoEnv>();
+todos.use(authMiddleware);
+
+todos.get("/", async (c) => {
+  const user = c.get("user");
+  try {
+    const todos = await getTodosByUserID(user.id);
+    return c.json(todos);
+  } catch (error) {
+    console.error("Failed to fetch todos: ", error);
+    return c.json({ error: "Failed to fetch todos" }, 500);
+  }
+});
